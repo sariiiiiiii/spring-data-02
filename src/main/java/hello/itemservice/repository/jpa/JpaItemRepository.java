66c7eaa8611efa773356ff09e71661689f54a1b6
sari <abdf13423@naver.com>
2023-09-30 19:18:39 +0900

    spring data 02

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -7,8 +7,10 @@ import hello.itemservice.repository.ItemUpdateDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,7 +38,7 @@ public class JpaItemRepository implements ItemRepository {
 
         /**
          * 변경감지 (dirty checking)
-         * JPA 는 조회시에 미리 스냅샷을 떠놓고 트랜잭션이 끝나는 시점에
+         * JPA 는 조회시에 미리 스냅샷을 떠놓고 트랜잭션이 커밋이 되는 시점에
          * 같은 식별자로 된 데이터에 변경된 점이 있으면 update query 가 나감.
          */
 
@@ -54,7 +56,32 @@ public class JpaItemRepository implements ItemRepository {
 
     @Override
     public List<Item> findAll(ItemSearchCondition cond) {
-        return null;
+        String jpql = "select i from Item i";
+        Integer maxPrice = cond.getMaxPrice();
+        String itemName = cond.getItemName();
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            jpql += " where";
+        }
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            jpql += " i.itemName like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                jpql += " and";
+            }
+            jpql += " i.price <= :maxPrice";
+        }
+        log.info("jpql={}", jpql);
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+        if (StringUtils.hasText(itemName)) {
+            query.setParameter("itemName", itemName);
+        }
+        if (maxPrice != null) {
+            query.setParameter("maxPrice", maxPrice);
+        }
+        return query.getResultList();
     }
 
 }
